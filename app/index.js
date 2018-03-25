7b0a180d17b0192f68ffb8f697eb73c9e9e12bd6
navinprasadk <NA351942@wipro.com>
2018-03-25 20:06:08 +0530

    Themes Screen added

--- app/index.js
+++ app/index.js
@@ -3,13 +3,18 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 import Home from './screens/Home';
 import CurrencyList from './screens/CurrencyList';
 import Options from './screens/Options';
+import Themes from './screens/Themes';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
+  $primaryOrange: '#D57A66',
+  $primaryGreen: '#00BD9D',
+  $primaryPurple: '#9E768F',
+
   $white: '#fff',
   $border: '#E2E2E2',
   $inputText: '#797979',
   $lightGray: '#F0F0F0',
   $darkText: '#343434',
 });
-export default () => <Options />;
+export default () => <Themes />;
